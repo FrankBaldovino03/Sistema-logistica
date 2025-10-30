1bf95715afff60160daa01d55e15c0a1ae5efd2d
FrankBaldovino03 <baldovinofranklin0406@gmail.com>
2025-10-29 21:12:13 -0500

    Actualización de EnvioAereo y ajustes varios

--- src/EnvioAereo.java
+++ src/EnvioAereo.java
@@ -4,7 +4,6 @@ public class EnvioAereo extends Envio {
     private static final double COSTO_POR_KG = 15000.0;
     private static final double RECARGO_INTERNACIONAL = 50000.0;
     private static final double RECARGO_EXPRESS = 0.8; // 80%
-    private static final double PESO_MAXIMO = 1000.0;
 
     public EnvioAereo(String origen, String destino, double peso, String prioridad, String tipoEnvio,
             boolean esInternacional) {
